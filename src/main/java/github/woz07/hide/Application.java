21a5d52430aba2e0c086ac9d08ba71da97df4d24
woz07 <105782312+woz07@users.noreply.github.com>
2024-05-27 23:24:47 +0100

    ADD: Base app, no features, just frame

--- src/main/java/github/woz07/hide/Application.java
+++ src/main/java/github/woz07/hide/Application.java
@@ -1,17 +1,38 @@
 package github.woz07.hide;
 
+import com.formdev.flatlaf.themes.FlatMacDarkLaf;
 import github.woz07.BCipher;
+import github.woz07.liteconfig.Configuration;
 
 import javax.swing.*;
+import java.awt.*;
+import java.io.File;
 
 public class Application extends JFrame {
-    private final BCipher bCipher;
+    private static final Configuration config = new Configuration(new File("config.txt"));
+    private static final BCipher bCipher = new BCipher();
+    
+//    private final JPanel container;
     public Application() {
-        bCipher = new BCipher();
+        setTitle("Hide");
+        setPreferredSize(new Dimension(600, 400));
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
         
+        pack();
+        setLocationRelativeTo(null);
+        setResizable(false);
+        setVisible(true);
     }
     
     public static void main(String[] args) {
-        new Application();
+        // Set mode
+        if (config.get("MODE").equals("light")) {
+            FlatMacDarkLaf.setup();
+        } else {
+            FlatMacDarkLaf.setup();
+        }
+        
+        // Run application
+        SwingUtilities.invokeLater(Application::new);
     }
 }
