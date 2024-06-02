dec6c8678775a3f3faf0549b911d1c5ce7f0f344
woz07 <105782312+woz07@users.noreply.github.com>
2024-06-02 21:42:44 +0100

    ADD: WError, a frame that displays the error and some support so no output is sent to terminal

---
+++ src/main/java/github/woz07/hide/WError.java
@@ -0,0 +1,47 @@
+package github.woz07.hide;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class WError extends JFrame {
+    // l = Label
+    private final JLabel lError;
+    // b = Button
+    private final JButton bAccept;
+    private final JButton bHelp;
+    public WError(Frame parent, Exception exception) {
+        setTitle("Hide ~ Error");
+        setPreferredSize(new Dimension(320, 200));
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        
+        // Setting up components
+        lError = new JLabel(exception.getMessage());
+        
+        bAccept = new JButton("Okay");
+        bAccept.addActionListener(e -> dispose());
+        bHelp = new JButton("Help");
+        bHelp.addActionListener(e -> {
+            try {
+                URI uri = new URI("https://www.github.com/woz07/hide#Errors");
+                Desktop desktop = Desktop.getDesktop();
+                if (desktop.isSupported(Desktop.Action.BROWSE)) {
+                    desktop.browse(uri);
+                }
+            } catch (URISyntaxException | IOException u) {
+                u.printStackTrace();
+            }
+        });
+        
+        // Finalizing components
+        
+        pack();
+        setLocationRelativeTo(parent);
+        setResizable(false);
+        setVisible(true);
+    }
+}
